df41e05ac8fc53ec75ab76d7992a719c7ce02934
alexnsouza6 <alexnsouza6@hotmail.com>
2019-04-15 09:57:40 -0300

    feat: setup react router with switch and router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Loader } from "semantic-ui-react";
+import Pokemon from "./components/Pokemon/Pokemon";
 
-const App = () => <div className="App">Hello, world!</div>;
+const PokeDex = lazy(() => import("./components/Pokedex/PokeDex"));
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <Suspense fallback={<Loader active inline="centered" />}>
+              <PokeDex {...props} />
+            </Suspense>
+          )}
+        />
+        <Route path="/pokemon/:id" render={props => <Pokemon {...props} />} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
