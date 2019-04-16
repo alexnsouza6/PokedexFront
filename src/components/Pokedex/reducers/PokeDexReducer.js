1a2814b5a92aa958805d563c13e7baea952fe0cc
alexnsouza6 <alexnsouza6@hotmail.com>
2019-04-15 23:19:43 -0300

    feat: case for pokemon edition

--- src/components/Pokedex/reducers/PokeDexReducer.js
+++ src/components/Pokedex/reducers/PokeDexReducer.js
@@ -1,10 +1,16 @@
-import FETCH_POKEMON from "../actions/consts";
+import { FETCH_POKEMON, POKEMON_CREATE, POKEMON_EDIT } from "../actions/consts";
 const initialState = {};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case FETCH_POKEMON:
       return { ...state, byID: [...payload] };
+
+    case POKEMON_CREATE:
+      return { ...state, ...payload };
+
+    case POKEMON_EDIT:
+      return { ...state, ...payload };
 
     default:
       return state;
