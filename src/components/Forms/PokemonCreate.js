a1367e48fdd1616a11eb4b584c33055ab30f7670
alexnsouza6 <alexnsouza6@hotmail.com>
2019-04-15 23:18:19 -0300

    feat: adds evolution input changing

---
+++ src/components/Forms/PokemonCreate.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { Button, Header } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { createPokemon } from "./actions/PokemonActions";
+
+class PokemonCreate extends Component {
+  state = {
+    pokemonName: "",
+    pokemonImage: {},
+    pokemonType: "",
+    pokemonEvolutionID: ""
+  };
+
+  handlePokemonNameChange = e => {
+    this.setState({ pokemonName: e.target.value });
+  };
+
+  handlePokemonType = e => {
+    this.setState({ pokemonType: e.target.value });
+  };
+
+  handlePokemonEvolution = e => {
+    this.setState({ pokemonEvolutionID: parseInt(e.target.value) });
+  };
+
+  fileSelectedHandler = event => {
+    const selectedFile = event.target.files;
+    this.setState({ pokemonImage: selectedFile[0] });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const type = [
+      { description: this.state.pokemonType },
+      { description: "grass" }
+    ];
+    const fd = new FormData();
+    fd.append("image_url", this.state.pokemonImage);
+    fd.append("name", this.state.pokemonName);
+    fd.append("evolution_id", this.state.pokemonEvolutionID);
+    fd.append("pokemon_types[]", JSON.stringify(type));
+    this.props.createPokemon(fd, () => this.props.history.push("/"));
+  };
+
+  render() {
+    return (
+      <div>
+        <Header as="h1"> Create your own pokemon! </Header>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor={this.state.pokemonName}> Pokemon Name </label>
+          <input
+            type="text"
+            value={this.state.pokemonName}
+            onChange={this.handlePokemonNameChange}
+          />
+          <br />
+          <label htmlFor="pokemon-image"> Pokemon Picture </label>
+          <input type="file" onChange={this.fileSelectedHandler} />
+          <br />
+          <label htmlFor={this.state.pokemonType}>Pokemon Type</label>
+          <input
+            type="text"
+            value={this.state.pokemonType}
+            onChange={this.handlePokemonType}
+          />
+          <br />
+          <label htmlFor={this.state.pokemonEvolutionID}>
+            Pokemon Evolution (Type its ID)
+          </label>
+          <input
+            type="text"
+            value={this.state.pokemonEvolutionID}
+            onChange={this.handlePokemonEvolution}
+          />
+          <br />
+          <Button type="submit"> Submit </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { createPokemon }
+)(PokemonCreate);
---
+++ src/components/Forms/PokemonEdit.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { Button, Form, Header } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { editPokemon } from "./actions/PokemonActions";
+
+class PokemonEdit extends Component {
+  state = {
+    pokemonName: "",
+    pokemonImage: {},
+    pokemonEvolutionID: ""
+  };
+
+  handlePokemonNameChange = e => {
+    this.setState({ pokemonName: e.target.value });
+  };
+
+  fileSelectedHandler = event => {
+    const selectedFile = event.target.files;
+    this.setState({ pokemonImage: selectedFile[0] });
+  };
+
+  handlePokemonEvolution = e => {
+    this.setState({ pokemonEvolutionID: parseInt(e.target.value) });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const fd = new FormData();
+    fd.append("image_url", this.state.pokemonImage);
+    fd.append("name", this.state.pokemonName);
+    fd.append("evolution_id", this.state.pokemonEvolutionID);
+    this.props.editPokemon(fd, this.props.match.params.id, () =>
+      this.props.history.push("/")
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <Header as="h1"> Edit this pokemon! </Header>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor={this.state.pokemonEvolutionID}>Pokemon Name: </label>
+          <input
+            type="text"
+            value={this.state.pokemonName}
+            onChange={this.handlePokemonNameChange}
+          />
+          <br />
+          <label htmlFor={this.state.pokemonEvolutionID}>Pokemon Image: </label>
+          <input type="file" onChange={this.fileSelectedHandler} />
+          <br />
+          <label htmlFor={this.state.pokemonEvolutionID}>
+            Pokemon Evolution (Type its ID):
+          </label>
+          <input
+            type="text"
+            value={this.state.pokemonEvolutionID}
+            onChange={this.handlePokemonEvolution}
+          />
+          <br />
+          <Button type="submit"> Submit </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { editPokemon }
+)(PokemonEdit);
